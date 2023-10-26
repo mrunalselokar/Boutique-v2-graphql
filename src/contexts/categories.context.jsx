e2faad0eb2bc742fe6701cf5379a7dbfe89ab38b
swapnil-w <swapnil@webkorps.com>
2023-10-26 06:40:46 +0530

    using graphql and apollo client

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,27 +1,46 @@
 import { createContext, useState, useEffect } from 'react';
 
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+import { gql, useQuery } from '@apollo/client';
 
 export const CategoriesContext = createContext({
-  categoriesMap: {},
+	categoriesMap: {},
 });
 
+const COLLECTIONS = gql`
+	query GetCollections {
+		collections {
+			id
+			title
+			items {
+				id
+				name
+				price
+				imageUrl
+			}
+		}
+	}
+`;
+
 export const CategoriesProvider = ({ children }) => {
-  const [categoriesMap, setCategoriesMap] = useState({});
+	const [categoriesMap, setCategoriesMap] = useState({});
+	const { loading, error, data } = useQuery(COLLECTIONS);
 
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
+	useEffect(() => {
+		if (data) {
+			const { collections } = data;
+			const collectionsMap = collections.reduce((acc, collection) => {
+				const { title, items } = collection;
+				acc[title.toLowerCase()] = items;
+				return acc;
+			}, {});
+			setCategoriesMap(collectionsMap);
+		}
+	}, [data]);
 
-    getCategoriesMap();
-  }, []);
-
-  const value = { categoriesMap };
-  return (
-    <CategoriesContext.Provider value={value}>
-      {children}
-    </CategoriesContext.Provider>
-  );
+	const value = { categoriesMap, loading, error };
+	return (
+		<CategoriesContext.Provider value={value}>
+			{children}
+		</CategoriesContext.Provider>
+	);
 };
